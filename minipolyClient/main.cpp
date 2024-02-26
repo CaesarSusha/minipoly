2fbcc84738b0d2a0010a136992a770bca4ad1bfd
CaesarSusha <kaiser_alexandra@protonmail.com>
2024-02-26 09:52:27 +0100

    fixed QT network include

--- minipolyClient/main.cpp
+++ minipolyClient/main.cpp
@@ -1,11 +1,9 @@
 #include <QCoreApplication>
-#include "simpleServer.h"
-#include "simpleClient.h"
+#include "Client.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    Server server;
     Client client;
     client.start("127.0.0.1",8888);
 
